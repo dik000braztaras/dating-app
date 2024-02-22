4d41945952bcb3aad5fd5fea8e3c8c5dbbd2f562
Taras Sidorov <160868090+dik000braztaras@users.noreply.github.com>
2024-02-22 16:12:01 +0200

    Update match page.dart

--- match page.dart
+++ match page.dart
@@ -12,3 +12,19 @@ final sdkMessage = SDKIPCResponse.fromIPCMessage(message);
         'SDKIPCResponse{success: true, errorCode: , error: , data: $MOCK_USERNAME, action: ${Strings.GET_PROFILE}}',
         sdkMessage.toString());
   });
+
+  test('should give error response for the encoded message', () {
+    const message =
+        'ewoJImFjdGlvbiI6ICJnZXRQcm9maWxlIiwKCSJzdWNjZXNzIjogZmFsc2UsCgkiZXJyb3IiOiAiV2FsbGV0IGRvZXNuJ3QgZXhpc3RzIiwKCSJlcnJvckNvZGUiOiAic29tZXRoaW5nV2VudFdyb25nIiwKCSJkYXRhIjogIiIKfQ==';
+
+    final sdkMessage = SDKIPCResponse.fromIPCMessage(message);
+    expect(Strings.GET_PROFILE, sdkMessage.action);
+    expect(false, sdkMessage.success);
+    expect(true, sdkMessage.data.isEmpty);
+    expect('somethingWentWrong', sdkMessage.errorCode);
+    expect('Wallet doesn\'t exists', sdkMessage.error);
+    expect(
+        'SDKIPCResponse{success: false, errorCode: somethingWentWrong, error: Wallet doesn\'t exists, data: , action: ${Strings.GET_PROFILE}}',
+        sdkMessage.toString());
+  });
+}
